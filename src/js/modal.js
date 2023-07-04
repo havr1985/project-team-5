e6ffbb3c8fcaec18d2920074cafbf4bc3730fa15
havr1985 <gavrtel1985@gmail.com>
2023-07-04 18:19:39 +0300

    added script for buttons

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,29 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
+
+
+const modalButtons = document.querySelectorAll('[data-modal-open]');
+const modal = document.querySelector('[data-modal]');
+const modalClose = document.querySelector('[data-modal-close]');
+
+modalButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    modal.classList.remove('is-hidden');
+  });
+});
+
+modalClose.addEventListener('click', function() {
+  modal.classList.add('is-hidden');
+});
